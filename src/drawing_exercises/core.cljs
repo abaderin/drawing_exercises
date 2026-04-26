415376671ee39b3afcfcb29606a2a1fd1f99eecb
Anton Baderin <anton.baderin@gcore.com>
2026-04-26 13:36:10 +0400

    infer-warnings were fixed

--- src/drawing_exercises/core.cljs
+++ src/drawing_exercises/core.cljs
@@ -20,14 +20,14 @@
     (set! (.-textContent el) text)
     (set! (.. el -style -color) color)))
 
-(defn configure-canvas! [context device format]
+(defn configure-canvas! [^js context device format]
   (.configure context #js
     {:device device
      :format format
      :usage (.-RENDER_ATTACHMENT js/GPUTextureUsage)
      :alphaMode "opaque"}))
 
-(defn create-pipeline [device format vertex-code fragment-code]
+(defn create-pipeline [^js device format vertex-code fragment-code]
   (let [shader-module (.createShaderModule device #js {:code vertex-code})
         shader-module-frag (.createShaderModule device #js {:code fragment-code})]
     (.createRenderPipeline device #js
@@ -42,7 +42,7 @@
        :primitive #js
        {:topology "triangle-list"}})))
 
-(defn create-buffers [device]
+(defn create-buffers [^js device]
   (let [uniform-buffer (.createBuffer device #js
                          {:size 16
                           :usage (bit-or (.-UNIFORM js/GPUBufferUsage)
@@ -50,13 +50,13 @@
                           :mappedAtCreation false})]
     {:uniform uniform-buffer}))
 
-(defn create-bind-group [device pipeline uniform-buffer]
+(defn create-bind-group [^js device ^js pipeline uniform-buffer]
   (.createBindGroup device #js
     {:layout (.getBindGroupLayout pipeline 0)
      :entries #js [#js {:binding 0
                         :resource #js {:buffer uniform-buffer}}]}))
 
-(defn resize-canvas [canvas device context canvas-format]
+(defn resize-canvas [^js canvas device context canvas-format]
   (let [client-width (.-clientWidth canvas)
         client-height (.-clientHeight canvas)
         width (if (pos? client-width) client-width (.-width canvas))
@@ -69,7 +69,7 @@
     {:width width
      :height height}))
 
-(defn render [canvas device context pipeline bind-group uniform-buffer canvas-format time]
+(defn render [canvas ^js device ^js context pipeline bind-group uniform-buffer canvas-format time]
   (let [_ (resize-canvas canvas device context canvas-format)
         time-data (js/Float32Array. #js [time 0 0 0])
         command-encoder (.createCommandEncoder device)
@@ -80,23 +80,24 @@
                                                {:view texture-view
                                                 :clearValue #js {:r 0.1 :g 0.1 :b 0.1 :a 1.0}
                                                 :loadOp "clear"
-                                                :storeOp "store"}]})]
-    (-> device .-queue (.writeBuffer uniform-buffer 0 time-data))
+                                                :storeOp "store"}]})
+        queue (.-queue device)]
+    (.writeBuffer queue uniform-buffer 0 time-data)
     (.setPipeline render-pass pipeline)
     (.setBindGroup render-pass 0 bind-group)
     (.draw render-pass 3)
     (.end render-pass)
-    (-> device .-queue (.submit #js [(.finish command-encoder)]))))
+    (.submit queue #js [(.finish command-encoder)])))
 
 (defn animate [time]
   (let [{:keys [canvas device context pipeline bind-group buffers canvas-format render-error-shown?]} @app-state]
     (when device
-    (try
-      (render canvas device context pipeline bind-group (:uniform buffers) canvas-format (/ time 1000))
-      (catch :default err
-        (when-not render-error-shown?
-          (swap! app-state assoc :render-error-shown? true)
-          (set-status! (str "Render failed: " err) "#ff6b6b"))))
+      (try
+        (render canvas device context pipeline bind-group (:uniform buffers) canvas-format (/ time 1000))
+        (catch :default err
+          (when-not render-error-shown?
+            (swap! app-state assoc :render-error-shown? true)
+            (set-status! (str "Render failed: " err) "#ff6b6b"))))
       (swap! app-state assoc :animation-id (js/requestAnimationFrame animate)))))
 
 (defn ^:export init []
@@ -106,9 +107,9 @@
       (swap! app-state assoc :canvas (js/document.getElementById "canvas"))
 
       (-> js/navigator .-gpu (.requestAdapter nil)
-          (.then (fn [adapter]
+          (.then (fn [^js adapter]
                    (.requestDevice adapter nil)))
-          (.then (fn [dev]
+          (.then (fn [^js dev]
                    (let [cache-bust (.now js/Date)]
                      (-> (js/Promise.all #js [(load-shader (str "vertex.wgsl?v=" cache-bust))
                                               (load-shader (str "fragment.wgsl?v=" cache-bust))])
